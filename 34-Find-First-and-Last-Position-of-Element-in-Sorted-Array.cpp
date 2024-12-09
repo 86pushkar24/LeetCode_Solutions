4864de4150d559cc703dd9981d2991a8b1e76ce0
Pushkar Gupta <guptapushkar86@gmail.com>
2024-12-09 20:56:28 +0530

    [34] [Time Beats: 100.00%] [Memory Beats: 16.93%] - LeetPush

---
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
@@ -0,0 +1,90 @@
+// class Solution {
+// public:
+//     int lowerbound(vector<int> nums, int target, int high, int low) {
+//         int ans = nums.size();
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             // if (nums[mid] == target)
+//             //     return mid;
+//             if (nums[mid] >= target) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             } else
+//                 low = mid + 1;
+//         }
+//         return ans;
+//     }
+//     int upperbound(vector<int> nums, int target, int high, int low) {
+//         int ans = nums.size();
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+//             // if (nums[mid] == target)
+//             //     return mid;
+//             if (nums[mid] > target) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             } else
+//                 low = mid + 1;
+//         }
+//         return ans;
+//     }
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         if (nums.size() == 0)
+//             return {-1, -1};
+//         int low = 0, high = nums.size() - 1;
+//         int lb = lowerbound(nums, target, high, low);
+//         // cout << lb << \ \;
+//         // if(lb == nums.size()) lb = -1;
+//         if (lb == nums.size() || nums[lb] != target) {
+//             lb = -1;
+//         }
+//         int ub = upperbound(nums, target, high, low) - 1;
+//         // cout << ub << \ \;
+//         // if(ub == -1) ub = -1;
+//         if (ub == -1 || nums[ub] != target)ub = -1;
+//         return {lb, ub};
+//     }
+// };
+class Solution {
+public:
+    int lowerbound(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = nums.size();
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    int upperbound(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = nums.size();
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.empty()) return {-1, -1};
+
+        int lb = lowerbound(nums, target);
+        // Check if the lowerbound index actually points to the target
+        if (lb == nums.size() || nums[lb] != target) return {-1, -1};
+
+        int ub = upperbound(nums, target) - 1;
+        return {lb, ub};
+    }
+};
