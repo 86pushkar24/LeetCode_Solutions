b92e45d8bb0dd996110e7decd98a8f496acac20e
Pushkar Gupta <guptapushkar86@gmail.com>
2024-12-28 17:14:34 +0530

    [1283] [Time Beats: 29.45%] [Memory Beats: 57.23%] - LeetPush

---
+++ 1283-Find-the-Smallest-Divisor-Given-a-Threshold.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        // TC - O(N^2)
+        // for (double i = 1; i < INT_MAX; i++) {
+        //     int sum = 0;
+        //     for (int j = 0; j < nums.size(); j++) {
+        //         double k = ceil(nums[j] / i);
+        //         sum +=k;
+        //     }
+        //     if (sum <= threshold)
+        //         return i;
+        // }
+        // return 0;
+        int low = 0 , high = 1e6 , ans = 0;
+        while(low <= high){
+            double mid = low + (high - low) / 2;
+            double sum = 0;
+            for(int i = 0; i < nums.size(); i++){
+                sum += ceil(nums[i] / mid);
+                if(sum > threshold){
+                    break;
+                }
+            }
+            if(sum <= threshold){
+                high = mid - 1;
+                ans = mid;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+};
