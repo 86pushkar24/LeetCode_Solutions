e8168868e0937adae4dc0e623e4643a4cac8930f
Pushkar Gupta <guptapushkar86@gmail.com>
2024-12-29 13:57:09 +0530

    [875] [Time Beats: 46.60%] [Memory Beats: 43.19%] - LeetPush

--- 875-Koko-Eating-Bananas.cpp
+++ 875-Koko-Eating-Bananas.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1, high = *max_element(piles.begin(),piles.end());
-        int ans = high;
+        // int ans = high;
         while(low <= high){
             int mid = low + (high - low) / 2;
             int sum = 0;
@@ -14,10 +14,11 @@ public:
             cout << endl;
             if(sum <= h){
                 high = mid - 1;
-                ans = mid;
+                // ans = mid;
             }
             else low = mid + 1;
         }
-        return ans;
+        // return ans;
+        return low;
     }
 };
