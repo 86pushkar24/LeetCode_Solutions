1d3d9cbc30b04526a7cbf3e102ca084eafeefe47
Pushkar Gupta <guptapushkar86@gmail.com>
2025-04-14 01:27:44 +0530

    [2033] [Time Beats: 56.56%] [Memory Beats: 17.82%] - LeetPush

---
+++ Minimum-Operations-to-Make-a-Uni-Value-Grid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> v;  // don't pre-size it
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                v.push_back(grid[i][j]);
+            }
+        }
+        sort(v.begin(), v.end());
+        int median = v[v.size() / 2];
+        int cnt = 0;
+        for (int i = 0; i < v.size(); i++) {
+            int diff = abs(v[i] - median);
+            if (diff % x != 0) return -1; // not possible to equalize
+            cnt += diff / x;
+        }
+        return cnt;
+    }
+};
