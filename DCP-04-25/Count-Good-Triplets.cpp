15ac9f85bd6b2a34e93ce29740f3958d544e8610
Pushkar Gupta <guptapushkar86@gmail.com>
2025-04-14 11:23:38 +0530

    [1534] [Time Beats: 90.51%] [Memory Beats: 6.43%] - LeetPush

--- DCP-04-25/Count-Good-Triplets.cpp
+++ DCP-04-25/Count-Good-Triplets.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int cnt = 0;
-        for(int i = 0; i < arr.size()-2;i++){
-            for(int j = i+1;j< arr.size()-1;j++){
-                if(abs(arr[i]-arr[j])<= a){
-                    for(int k = j+1;k< arr.size();k++){
-                        if(abs(arr[j]-arr[k])<= b && abs(arr[i]-arr[k])<= c){
-                            cnt++;
-                            cout << i << " " << j << " " << k;
+        int cnt=0,n=arr.size();
+        vector<int> sum(1001,0);
+        for(int j = 0; j < n; j++){
+            for(int k = j+1; k <n;k++){
+                if(abs(arr[j]-arr[k])<=b){
+                    int l=max(0,max(arr[j]-a,arr[k]-c));
+                    int r=min(1000,min(arr[j]+a,arr[k]+c));
+                    if(l<=r){
+                        if(l==0){
+                            cnt+=sum[r];
+                        }
+                        else{
+                            cnt+=sum[r]-sum[l-1];
                         }
                     }
                 }
+            }
+            for(int i = arr[j]; i <= 1000; i++){
+                sum[i]++;
             }
         }
         return cnt;
